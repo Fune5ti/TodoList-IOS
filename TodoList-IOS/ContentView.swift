ca37b2c3ba7d01b7e73ae9b9cdc71a91c20e50fb
Daniel Pires <daniel.pires@devgo.org>
2021-04-04 17:30:34 -0100

    Initial Commit

---
+++ TodoList-IOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TodoList-IOS
+//
+//  Created by Daniel Pires on 04/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TodoList-IOS/TodoList_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoList_IOSApp.swift
+//  TodoList-IOS
+//
+//  Created by Daniel Pires on 04/04/21.
+//
+
+import SwiftUI
+
+@main
+struct TodoList_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
